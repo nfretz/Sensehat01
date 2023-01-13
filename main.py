5bde60c918995bec4dcff2093db7fc473d87e2f9
nfretz <nfretz@okanagan.bc.ca>
2023-01-12 20:52:43 -0800

    Added colour set

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 
-sense.show_message("Hello World")
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+yellow = (255,255,0)
+magenta = (255,0,255)
+cyan = (0,255,255)
+
+cblue = (0,32,91)
+cgreen = (10,134,61)
+
+sense.show_message("Go Canucks", text_colour=cblue, back_colour=cgreen)
+
+sense.clear(0,0,0)
