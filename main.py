0836bd20bd7094d8247c4537198cc09727229710
nfretz <nfretz@okanagan.bc.ca>
2023-01-16 11:34:00 -0800

    Oilers suck

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # Import based on using a physical RPi or the emulator
-from sense_emu import SenseHat
-# from sense_hat import SenseHat
+#from sense_emu import SenseHat
+from sense_hat import SenseHat
 sense = SenseHat()
 
 red = (255,0,0)
@@ -12,8 +12,10 @@ magenta = (255,0,255)
 cyan = (0,255,255)
 
 cblue = (0,32,91)
+oblue = (4,30,66)
 cgreen = (10,134,61)
+oorange = (252,76,0)
 
-sense.show_message("Go Canucks", text_colour=cblue, back_colour=cgreen)
+sense.show_message("Oilers suck", text_colour=oblue, back_colour=oorange)
 
 sense.clear(0,0,0)
