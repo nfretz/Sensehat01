bc362319e5f9644a98b248376fb65aeac53aefa0
nfretz <nfretz@okanagan.bc.ca>
2023-01-24 23:25:59 -0800

    Thingsboard with data

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 
+import json
+import requests
+from time import sleep
+
 # Import based on using a physical RPi or the emulator
 #from sense_emu import SenseHat
 from sense_hat import SenseHat
+
+# Constants
+
+API_KEY             = "jrAlYyNVNG1fYX6Woxky"
+THINGSBOARD_HOST    = "192.168.1.82:8080"
+
+thingsboard_url     = "http://{0}/api/v1/{1}/telemetry".format(THINGSBOARD_HOST, API_KEY)
+
 sense = SenseHat()
+data = {}
 
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-yellow = (255,255,0)
-magenta = (255,0,255)
-cyan = (0,255,255)
+while True:
+    data['temperature'] = sense.get_temperature()
+    data['pressure'] = sense.get_pressure()
+    data['humidity'] = sense.get_humidity()
+    r = requests.post(thingsboard_url, data=json.dumps(data))
+    print (str(data))
+    sleep(5)
 
-cblue = (0,32,91)
-oblue = (4,30,66)
-cgreen = (10,134,61)
-oorange = (252,76,0)
 
-sense.show_message("Oilers suck", text_colour=oblue, back_colour=oorange)
-
-sense.clear(0,0,0)
