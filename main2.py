1aa3fc788069ab8a7b7b39d8d166e1c546a13013
Nolan Fretz <nfretz@okanagan.bc.ca>
2025-02-02 22:10:09 -0800

    Gets new display message from TB

--- main2.py
+++ main2.py
@@ -8,12 +8,12 @@ import time
 from sense_emu import SenseHat
 
 # Constants
-INTERVAL            = 10  #Data capture and upload interval in seconds.
+INTERVAL            = 2  #Data capture and upload interval in seconds.
 API_KEY             = "bfsfmi1v6oo29qq69msr"   #Key for RPi
 THINGSBOARD_HOST    = "192.168.1.222:8080"
 
 telemetry_url     = "http://{0}/api/v1/{1}/telemetry".format(THINGSBOARD_HOST, API_KEY)
-attribute_url     = "http://{0}/api/v1/{1}/attributes/updates".format(THINGSBOARD_HOST, API_KEY)
+attribute_url     = "http://{0}/api/v1/{1}/attributes".format(THINGSBOARD_HOST, API_KEY)
 
 sense = SenseHat()
 data = {}
@@ -28,7 +28,9 @@ try:
         
         #Sending data to Thingsboard
         r = requests.post(telemetry_url, data=json.dumps(data))
-        
+        time.sleep(1)
+
+        #Receiving data from Thingsboard
         r = requests.get(attribute_url)
         msg = r.text
 
